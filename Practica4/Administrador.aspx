d91e391eba1ee7cc26e1cde15141c812ee8136f8
Romeo <misinicialesbray@gmail.com>
2019-04-18 20:21:14 -0600

    Pagina Inicio Administrador

---
+++ Practica4/Administrador.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Administrador.aspx.cs" Inherits="Practica4.Administrador" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server"> </head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>REGISTRO BANCO S.A.</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" /> 
+    <link href="CSS/estilos.css" rel="stylesheet" />
+<style>
+
+    body {
+    background-image: url(banco1.jpg);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    }
+</style>
+    <br />
+       <br /><br /><br />
+    
+    <center><img src="ad.png"></center>
+
+       <div class="container well contenedor" >
+        <div class="row">
+            <div class="col-xs-12">
+                <h2></h2>
+            </div>
+
+        </div>
+         <center><img src="eldeposito.png" style="height: 61px; width: 350px"><br />
+        <form runat="server" class="form-horizontal center-block">
+            <div class="form-group">
+                <asp:Label ID= "Cuenta" runat="server" Text="CUENTA" CssClass="control-label col-sm-2"></asp:Label> 
+                <div class="col-sm-10">
+                    <asp:ListBox  ID="TextBox1" runat="server" Cssclass="form-control" AutoPostBack="True" Height="28px" Rows="1">
+                        <asp:ListItem>SDFA</asp:ListItem>
+                        <asp:ListItem>SDAFDS</asp:ListItem>
+                        <asp:ListItem>SADFASD</asp:ListItem>
+                        <asp:ListItem>ASDF</asp:ListItem>
+                        <asp:ListItem>SADF</asp:ListItem>
+                    </asp:ListBox>
+                </div>
+
+                <asp:Label ID= "Monto" runat="server" Text="TOTAL" CssClass="control-label col-sm-2"></asp:Label> 
+                <div class="col-sm-10">
+                    <asp:TextBox ID="txtCuenta" runat="server" Cssclass="form-control">MONTO</asp:TextBox>
+                </div>
+            </div>
+            <div class="form-group">
+                <asp:Button ID="btnRealizarDeposito" runat="server" Text ="Depositar"  Cssclass="form-control btn btn-primary"  Width="247px"></asp:Button>
+            </div>
+            <br />
+            <br />
+            <br />
+        <center><img src="eldebito.png" style="height: 61px; width: 350px"><br />
+            <div class="form-group">
+                <asp:Label ID= "CUENTA2" runat="server" Text="CUENTA" CssClass="control-label col-sm-2"></asp:Label> 
+                <div class="col-sm-10">
+                    <asp:ListBox  ID="CUENTASD" runat="server" Cssclass="form-control" AutoPostBack="True" Height="28px" Rows="1">
+                        <asp:ListItem>SDFA</asp:ListItem>
+                        <asp:ListItem>SDAFDS</asp:ListItem>
+                        <asp:ListItem>SADFASD</asp:ListItem>
+                        <asp:ListItem>ASDF</asp:ListItem>
+                        <asp:ListItem>SADF</asp:ListItem>
+                    </asp:ListBox>
+                </div>
+
+                    <asp:Label ID= "DESC" runat="server" Text="INFO" CssClass="control-label col-sm-2"></asp:Label> 
+                    <div class="col-sm-10">
+                        <asp:TextBox ID="txtDescripcion" runat="server" Cssclass="form-control">DESCRIPCION</asp:TextBox>
+                    </div>
+                    <asp:Label ID= "Monto2" runat="server" Text="MONTO" CssClass="control-label col-sm-2"></asp:Label> 
+                    <div class="col-sm-10">
+                        <asp:TextBox ID="TextBox2" runat="server" Cssclass="form-control">MONTO</asp:TextBox>
+                        <br />
+                    </div>
+                <br />
+                <br />
+                    <div class="form-group">
+                        <asp:Button ID="Debitar" runat="server" Text ="Debitar"  Cssclass="form-control btn btn-primary"  Width="247px"></asp:Button>
+                    </div>
+
+
+                <br />
+                <br />
+                    <div class="form-group">
+                        <asp:Button ID="LOSCREDITOS" runat="server" Text ="Gestión de Creditos"  Cssclass="form-control btn btn-primary"  Width="247px"></asp:Button>
+                    </div>
+               </div>
+     </form>
+
+       </div>
+
+<body>
+   
+    
+    
+</body>
+</html>
---
+++ Practica4/Administrador.aspx.designer.cs
@@ -0,0 +1,132 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Practica4 {
+    
+    
+    public partial class Administrador {
+        
+        /// <summary>
+        /// Control Cuenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Cuenta;
+        
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox TextBox1;
+        
+        /// <summary>
+        /// Control Monto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Monto;
+        
+        /// <summary>
+        /// Control txtCuenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCuenta;
+        
+        /// <summary>
+        /// Control btnRealizarDeposito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRealizarDeposito;
+        
+        /// <summary>
+        /// Control CUENTA2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label CUENTA2;
+        
+        /// <summary>
+        /// Control CUENTASD.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox CUENTASD;
+        
+        /// <summary>
+        /// Control DESC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DESC;
+        
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
+        
+        /// <summary>
+        /// Control Monto2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Monto2;
+        
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// Control Debitar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Debitar;
+        
+        /// <summary>
+        /// Control LOSCREDITOS.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button LOSCREDITOS;
+    }
+}
