8a1a6fabfb81e9c7fbb9bf5fce1da50dbc6374e3
Romeo <misinicialesbray@gmail.com>
2019-04-23 22:46:03 -0600

    Pruebas Login

---
+++ Practica4/LoginM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Practica4
+{
+    public class LoginM
+    {
+
+        public Boolean Ingreso(String txtUsuario, String txtPassword, String txtCodigo)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog = Practica3_AYD1;Trusted_Connection=true;");
+            con.Open();
+            SqlCommand com = new SqlCommand(); // Create a object of SqlCommand class
+            com.Connection = con; //Pass the connection object to Command
+            com.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+            com.CommandText = "Login"; //Stored Procedure Name
+            com.Parameters.Add("@codigo_usuario", SqlDbType.NVarChar).Value = Convert.ToInt32(txtCodigo);
+            com.Parameters.Add("@usuario", SqlDbType.NVarChar).Value = txtUsuario;
+            com.Parameters.Add("@password", SqlDbType.NVarChar).Value = txtPassword;
+            com.ExecuteNonQuery();
+            int codigo1 = 0;
+            String Usuario = "";
+            String Passwordo = "";
+            SqlDataReader reader = com.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    codigo1 = reader.GetInt32(0);
+                    Usuario = reader.GetString(1);
+                    Passwordo = reader.GetString(2);
+                    break;
+                }
+            }
+            if (codigo1 != 0 && Usuario.Length > 0 && Passwordo.Length > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- TestWeb/UnitTest.cs
+++ TestWeb/UnitTest.cs
@@ -7,7 +7,7 @@ using System.Web;
 namespace TestWeb
 {
     [TestClass]
-    public class UnitTestRegistro
+    public class UnitTest
     {
         [TestMethod]
         public void PruebaAlfanumerica()
@@ -20,6 +20,7 @@ namespace TestWeb
         [TestMethod]
         public void LargoPalabra()
         {
+            //Verificando los metodos de registro
             var Registro = new RegistroM();
             var Servicio2 = Registro.Largo("12345678");
             if (Convert.ToInt32(Servicio2) == 8)
@@ -35,10 +36,19 @@ namespace TestWeb
         [TestMethod]
         public void ExistenciaUsuario()
         {
+            //Verificando los metodos de registro
             var Registro = new RegistroM();
             var Servicio3 = Registro.ExistenciaUsuario("rome123");
             Assert.IsTrue(true);
 
+        }
+
+        [TestMethod]
+        public void LoginTest()
+        {
+            var Login = new LoginM();
+            var Servicio1Login = Login.Ingreso("rome123", "1234asdf", "1022");
+            Assert.IsTrue(true);
         }
     }
 }
