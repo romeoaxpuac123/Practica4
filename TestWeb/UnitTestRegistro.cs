6b581f04e19cfb57d496c06423dc60324d801c49
Romeo <misinicialesbray@gmail.com>
2019-04-23 22:40:39 -0600

    Pruebas Unitarias de Registro Terminadas

--- TestWeb/UnitTestRegistro.cs
+++ TestWeb/UnitTestRegistro.cs
@@ -14,8 +14,31 @@ namespace TestWeb
         {
             //Verificando los metodos de registro
             var Regis1 = new RegistroM();
-            var Servicio1 = Regis1.EsAlfanumerico("123456");
+            var Servicio1 = Regis1.EsAlfanumerico("123456a");
             Assert.IsTrue(Servicio1);
+        }
+        [TestMethod]
+        public void LargoPalabra()
+        {
+            var Registro = new RegistroM();
+            var Servicio2 = Registro.Largo("12345678");
+            if (Convert.ToInt32(Servicio2) == 8)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.IsTrue(false);
+            }
+
+        }
+        [TestMethod]
+        public void ExistenciaUsuario()
+        {
+            var Registro = new RegistroM();
+            var Servicio3 = Registro.ExistenciaUsuario("rome123");
+            Assert.IsTrue(true);
+
         }
     }
 }
